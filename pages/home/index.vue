5c3d04440ef13663a8730b4aa9c5399a5449c7e8
zhangjiaqi24 <1us_z8wo1mw0ml@dingtalk.com>
2023-07-28 13:20:02 +0800

    feature: 首页搭建

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -81,7 +81,7 @@ export default {
     }
   },
   mounted() {
-
+    console.log(this.$store)
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,18 @@
 import Home from "~/pages/home";
 export default {
   name: 'IndexPage',
-  components: {Home}
+  components: {Home},
+  data() {
+    return {
+      //博客
+      blogList: []
+    }
+  },
+  mounted() {
+    console.log(this.title);
+  },
+  asyncData(data, callback) {
+    callback(null, {title: "2222"})
+  }
 }
 </script>
